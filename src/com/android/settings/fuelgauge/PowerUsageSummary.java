b7baf05f3b1e618d91c63f0e3d148a35be608d09
Lars Greiss <kufikugel@googlemail.com>
2014-12-11 18:20:40 +0100

    Settings: make unaccounted and over-counted battery usage configurable
    Google disabled it for release builds here:
    
    https://github.com/SlimRoms/packages_apps_Settings/commit/e6793771cedb47aed72f1c64f870b70357746938
    
    well but it is enabled for whatever other type of builds beside the official release builds. That said it should
    be a developer option so or so and not build type dependent.
    
    Whatever added an option into developer settings to control it. Default value is false.
    
    That said.....finally no complain about misc battery stat in battery settings xD
    
    Change-Id: I7549b1c796d5d9413b4af5d80a31d6efeaf0352a
    Signed-off-by: xdevs23 <xdevs23@outlook.com>

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -17,9 +17,13 @@
 package com.android.settings.fuelgauge;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.os.BatteryStats;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -27,6 +31,7 @@ import android.os.Process;
 import android.os.UserHandle;
 import android.preference.Preference;
 import android.preference.PreferenceGroup;
+import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
 import android.text.TextUtils;
 import android.util.SparseArray;
@@ -39,6 +44,7 @@ import android.view.MenuItem;
 import com.android.internal.os.BatterySipper;
 import com.android.internal.os.BatterySipper.DrainType;
 import com.android.internal.os.PowerProfile;
+import com.android.settings.DevelopmentSettings;
 import com.android.settings.HelpUtils;
 import com.android.settings.R;
 import com.android.settings.Settings.HighPowerApplicationsActivity;
@@ -287,6 +293,10 @@ public class PowerUsageSummary extends PowerUsageBase {
         mAppListGroup.setOrderingAsAdded(false);
         boolean addedSome = false;
 
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        final boolean showUnacAndOvercounted = sp.getBoolean(
+                DevelopmentSettings.SHOW_UNAC_AND_OVERCOUNTED_STATS, false);
+
         final PowerProfile powerProfile = mStatsHelper.getPowerProfile();
         final BatteryStats stats = mStatsHelper.getStats();
         final double averagePower = powerProfile.getAveragePower(PowerProfile.POWER_SCREEN_FULL);
@@ -322,12 +332,12 @@ public class PowerUsageSummary extends PowerUsageBase {
                     if (percentOfTotal < 10) {
                         continue;
                     }
-                    if ("user".equals(Build.TYPE)) {
+                    if (!showUnacAndOvercounted) {
                         continue;
                     }
                 }
                 if (sipper.drainType == BatterySipper.DrainType.UNACCOUNTED) {
-                    // Don't show over-counted unless it is at least 1/2 the size of
+                    // Don't show unacccounted unless it is at least 1/2 the size of
                     // the largest real entry, and its percent of total is more significant
                     if (sipper.totalPowerMah < (mStatsHelper.getMaxRealPower()/2)) {
                         continue;
@@ -335,7 +345,7 @@ public class PowerUsageSummary extends PowerUsageBase {
                     if (percentOfTotal < 5) {
                         continue;
                     }
-                    if ("user".equals(Build.TYPE)) {
+                    if (!showUnacAndOvercounted) {
                         continue;
                     }
                 }
