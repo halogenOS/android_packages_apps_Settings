d0d010cc59cae53fb32ca8734737e87b7e907f36
xdevs23 <xdevs23@outlook.com>
2016-05-22 19:43:42 +0200

    SystemUI: setting to enable/disable searchbar (Recents)
    Add a setting to enable or disable the search bar in recents view.

---
+++ src/com/android/settings/xos/settings/RecentsViewSettings.java
@@ -0,0 +1,183 @@
+/*
+ * Copyright (C) 2016 halogenOS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.xos.settings;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.view.RotationPolicy;
+import com.android.settings.DropDownPreference.Callback;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+import com.android.settings.R;
+
+import static android.provider.Settings.Global.RECENTS_SEARCHBAR_ENABLED;
+
+import android.app.Activity;
+import android.app.ActivityManagerNative;
+import android.app.Dialog;
+import android.app.UiModeManager;
+import android.app.admin.DevicePolicyManager;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.SystemProperties;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecentsViewSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener, OnPreferenceClickListener, Indexable {
+    private static final String TAG = "RecentsViewSettings";
+    private static final int DLG_GLOBAL_CHANGE_WARNING = 1;
+    
+    private static final String 
+                KEY_SHOW_SEARCHBAR = "enable_searchbar"
+                ;
+    
+    private final Configuration mCurConfig = new Configuration();
+    
+    private SwitchPreference 
+                mShowSearchbarPreference
+                ;
+    
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.DISPLAY;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final Activity activity = getActivity();
+        final ContentResolver resolver = activity.getContentResolver();
+
+        addPreferencesFromResource(R.xml.recentsview_settings);
+        
+        // Display -> Lockscreen settings -> Lockscreen visualizer
+        mShowSearchbarPreference = 
+            (SwitchPreference) findPreference(KEY_SHOW_SEARCHBAR);
+        mShowSearchbarPreference.setOnPreferenceChangeListener(this);
+        updateState();
+    }
+
+    int floatToIndex(float val) {
+        String[] indices = getResources().getStringArray(R.array.entryvalues_font_size);
+        float lastVal = Float.parseFloat(indices[0]);
+        for (int i=1; i<indices.length; i++) {
+            float thisVal = Float.parseFloat(indices[i]);
+            if (val < (lastVal + (thisVal-lastVal)*.5f)) {
+                return i-1;
+            }
+            lastVal = thisVal;
+        }
+        return indices.length-1;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateState();
+    }
+
+    @Override
+    public Dialog onCreateDialog(int dialogId) {
+        if (dialogId == DLG_GLOBAL_CHANGE_WARNING) {
+            return Utils.buildGlobalChangeWarningDialog(getActivity(),
+                    R.string.global_font_change_title,
+                    new Runnable() {
+                        public void run() {
+                        }
+                    });
+        }
+        return null;
+    }
+
+    private void updateState() {
+        mShowSearchbarPreference.setChecked(
+            Settings.Global.getInt(getContentResolver(), RECENTS_SEARCHBAR_ENABLED, 0) == 1
+        );
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+
+    /**
+     * Add you preference handlers here.
+     * Don't create custom ones.
+     */
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        final String key = preference.getKey();
+        // Display -> Lockscreen settings -> Lockscreen visualizer
+        if (preference == mShowSearchbarPreference) {
+            Settings.Global.putInt(getContentResolver(), RECENTS_SEARCHBAR_ENABLED,
+                ((Boolean)objValue));
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        return false;
+    }
+
+    @Override
+    protected int getHelpResource() {
+        return R.string.help_uri_display;
+    }
+
+    public static final Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+                @Override
+                public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
+                        boolean enabled) {
+                    ArrayList<SearchIndexableResource> result =
+                            new ArrayList<SearchIndexableResource>();
+
+                    SearchIndexableResource sir = new SearchIndexableResource(context);
+                    sir.xmlResId = R.xml.notification_panel_tweaks_settings;
+                    result.add(sir);
+
+                    return result;
+                }
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    ArrayList<String> result = new ArrayList<String>();
+
+                    return result;
+                }
+            };
+}
