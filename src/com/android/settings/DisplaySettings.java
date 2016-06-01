bca0d383be7bd0ac5b75902c5c47b286de187b6d
xdevs23 <xdevs23@outlook.com>
2016-06-01 17:48:37 +0200

    Heads-Up: add setting to enable/disable heads-up [2/2]
    Change-Id: I6863f65fb76e14c8dfde5b04b32b3c4d038dd572
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -117,6 +117,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String SHOW_NETWORK_NAME_MODE = "show_network_name_mode";
     private static final String KEY_LOCKSCREEN_VISUALIZER = "lockscreen_visualizer";
     private static final String KEY_DOUBLE_TAP_SLEEP = "double_tap_sleep_gesture";
+    private static final String KEY_HEADSUP = "enable_headsup";
     private static final int SHOW_NETWORK_NAME_ON = 1;
     private static final int SHOW_NETWORK_NAME_OFF = 0;
 
@@ -142,6 +143,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private SwitchPreference mNetworkNameDisplayedPreference = null;
     private SwitchPreference mLockscreenVisualizerPreference;
     private SwitchPreference mDoubleTapSleepPreference;
+    private SwitchPreference mHeadsupPreference;
 
     private SharedPreferences mSharedPreferences;
     private SharedPreferences.Editor mEditor;
@@ -323,6 +325,9 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         mDoubleTapSleepPreference = (SwitchPreference)
                             findPreference(KEY_DOUBLE_TAP_SLEEP);
         mDoubleTapSleepPreference.setOnPreferenceChangeListener(this);
+
+        mHeadsupPreference = (SwitchPreference) findPreference(KEY_HEADSUP);
+        mHeadsupPreference.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -516,6 +521,12 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
                     Settings.System.DOUBLE_TAP_SLEEP_GESTURE,
                     1, UserHandle.USER_CURRENT) == 1);
         }
+
+        if (mHeadsupPreference != null)
+            mHeadsupPreference.setChecked(
+                Settings.System.getIntForUser(getContentResolver(),
+                    Settings.System.KEY_ENABLE_HEADSUP_NOTIFICATIONS,
+                    1, UserHandle.USER_CURRENT) == 1);
     }
 
     private void updateScreenSaverSummary() {
@@ -604,6 +615,11 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         if(preference == mDoubleTapSleepPreference) {
             Settings.System.putIntForUser(getContentResolver(),
                 Settings.System.DOUBLE_TAP_SLEEP_GESTURE,
+                (boolean)objValue ? 1 : 0, UserHandle.USER_CURRENT);
+        }
+        if (preference == mHeadsupPreference) {
+            Settings.System.putIntForUser(getContentResolver(),
+                Settings.System.KEY_ENABLE_HEADSUP_NOTIFICATIONS,
                 (boolean)objValue ? 1 : 0, UserHandle.USER_CURRENT);
         }
         return true;
