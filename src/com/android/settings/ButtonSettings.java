225663170a3f6d25b6904b927cb00e03fb961e41
xdevs23 <xdevs23@outlook.com>
2016-07-15 17:52:25 +0200

    ButtonSettings: add navigation bar switch

--- src/com/android/settings/ButtonSettings.java
+++ src/com/android/settings/ButtonSettings.java
@@ -24,12 +24,14 @@ import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.os.Handler;
+import android.os.SystemProperties;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceManager;
 import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
 import android.provider.Settings;
 
 import android.util.Log;
@@ -53,6 +55,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private static final String KEY_HOME_DOUBLE_TAP = "hardware_keys_home_double_tap";
     private static final String KEY_MENU_PRESS = "hardware_keys_menu_press";
     private static final String KEY_MENU_LONG_PRESS = "hardware_keys_menu_long_press";
+    private static final String KEY_NAVBAR_SWITCH = "navigationbar_switch";
 
     private static final String CATEGORY_POWER = "power_key";
     private static final String CATEGORY_HOME = "home_key";
@@ -90,6 +93,8 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     private ListPreference mHomeDoubleTapAction;
     private ListPreference mMenuPressAction;
     private ListPreference mMenuLongPressAction;
+    
+    private SwitchPreference mNavbarPreference;
 
     private PreferenceCategory mNavigationPreferencesCat;
 
@@ -171,6 +176,14 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         if (!backlight.isButtonSupported()) {
             prefScreen.removePreference(backlight);
         }
+        
+        mNavbarPreference = (SwitchPreference) findPreference(KEY_NAVBAR_SWITCH);
+        mNavbarPreference.setOnPreferenceChangeListener(this);
+        mNavbarPreference.setChecked(
+            Settings.System.getInt(getContentResolver(),
+                Settings.System.DEV_FORCE_SHOW_NAVBAR,
+                hasNavbarByDefault(getContext()) ? 1 : 0) == 1
+        );
     }
 
     private ListPreference initActionList(String key, int value) {
@@ -214,7 +227,29 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
                     Settings.System.KEY_MENU_LONG_PRESS_ACTION);
             return true;
         }
+        switch(preference.getKey()) {
+            case KEY_NAVBAR_SWITCH:
+                Settings.System.putInt(getContentResolver(),
+                    Settings.System.DEV_FORCE_SHOW_NAVBAR,
+                    (Boolean)newValue ? 1 : 0);
+                break;
+            default: break;
+        }
         return false;
+    }
+    
+    // Thanks https://goo.gl/JfubA9
+    private static boolean hasNavbarByDefault(Context context) {
+        Resources res;
+        try {
+            res = 
+                context.getPackageManager().getResourcesForApplication("android");
+        } catch(Exception ex) {
+            res = context.getResources();
+        }
+        int id = res.getIdentifier("config_showNavigationBar", "bool", "android");
+        String navBarOverride = SystemProperties.get("qemu.hw.mainkeys");
+        return Boolean.valueOf(res.getBoolean(id)) || "0".equals(navBarOverride);
     }
 
 }
