5ca5589e58c32d54330f2c6e8aa31e573eb0e7cb
Simao Gomes Viana <xdevs23@outlook.com>
2016-11-26 15:29:59 +0100

    Toggle for Modern Services
    Change-Id: I9ff8eb74506d3aba94ada7b36e9fb21de89edce8
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -214,6 +214,8 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
     private static final String KEY_CONVERT_FBE = "convert_to_file_encryption";
 
     private static final String OTA_DISABLE_AUTOMATIC_UPDATE_KEY = "ota_disable_automatic_update";
+    
+    private static final String KEY_ENABLE_MODERN_SERVICES = "modern_services";
 
     private static final int RESULT_DEBUG_APP = 1000;
     private static final int RESULT_MOCK_LOCATION_APP = 1001;
@@ -309,6 +311,8 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
     private SwitchPreference mForceResizable;
 
     private SwitchPreference mColorTemperaturePreference;
+    
+    private SwitchPreference mEnableModernServicesPreference;
 
     private final ArrayList<Preference> mAllPrefs = new ArrayList<Preference>();
 
@@ -521,6 +525,12 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
             removePreference(COLOR_TEMPERATURE_KEY);
             mColorTemperaturePreference = null;
         }
+        
+        mEnableModernServicesPreference = (SwitchPreference)
+                    findPreference(KEY_ENABLE_MODERN_SERVICES);
+        mEnableModernServicesPreference.setOnPreferenceChangeListener(this);
+        mEnableModernServicesPreference.setChecked(
+            SystemProperties.get("sf.enable_modern_services").equals("1"));
     }
 
     private ListPreference addListPreference(String prefKey) {
@@ -2157,6 +2167,14 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
             return true;
         } else if (preference == mSimulateColorSpace) {
             writeSimulateColorSpace(newValue);
+            return true;
+        } else if (preference == mEnableModernServicesPreference) {
+            SystemProperties.set("sf.enable_modern_services",
+                        (boolean)newValue ? "1" : "0");
+            Settings.System.putInt(getContentResolver(),
+                Settings.System.ENABLE_MODERN_SERVICES, (boolean)newValue ? 1 : 0);
+            Toast.makeText(getActivity(), R.string.modern_services_cleardata,
+                    Toast.LENGTH_LONG).show();
             return true;
         }
         return false;
