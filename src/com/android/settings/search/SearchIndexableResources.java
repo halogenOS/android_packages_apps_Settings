db2642a17737319607050000b2963f5d72146c05
Harsh Shandilya <msfjarvis@gmail.com>
2017-02-11 02:20:42 +0530

    Revert "Settings : Rename Customizations dashboard icon"
    This reverts commit 1c5db816f27b0f687871cfec3c50e63b9f10ebc0.
    
    Change-Id: I3eaa86b8ecbdab50901bf0ce44580e966c9afc2b
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- src/com/android/settings/search/SearchIndexableResources.java
+++ src/com/android/settings/search/SearchIndexableResources.java
@@ -340,7 +340,7 @@ public final class SearchIndexableResources {
                         Ranking.getRankForClassName(CustomizationsActivity.class.getName()),
                         NO_DATA_RES_ID,
                         CustomizationsActivity.class.getName(),
-                        R.drawable.ic_settings_customizations));
+                        R.drawable.ic_customizations));
     }
 
     private SearchIndexableResources() {
