21fe94738906a730fd26802e66b98c31a3f42c82
Harsh Shandilya <msfjarvis@gmail.com>
2017-10-25 01:30:56 +0530

    Settings: Add back custom summary to Customizations entry
    Change-Id: I1285694b0086e65f8e88022a04391ed10de11c7f
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- src/com/android/settings/customization/Customizations.java
+++ src/com/android/settings/customization/Customizations.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2010 The Android Open Source Project
- * Copyright (C) 2016 halogenOS
+ * Copyright (C) 2016-17 The halogenOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,13 +17,17 @@
 
 package com.android.settings.customization;
 
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.provider.SearchIndexableResource;
+import android.provider.Settings;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.core.PreferenceController;
 import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.dashboard.SummaryLoader;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -52,4 +56,37 @@ public class Customizations extends DashboardFragment {
     protected List<PreferenceController> getPreferenceControllers(Context context) {
         return null;
     }
+
+    private static class SummaryProvider implements SummaryLoader.SummaryProvider {
+
+        private final Context mContext;
+        private final SummaryLoader mSummaryLoader;
+        private ContentResolver resolver;
+
+        public SummaryProvider(Context context, SummaryLoader summaryLoader) {
+            mContext = context;
+            mSummaryLoader = summaryLoader;
+            resolver = mContext.getContentResolver();
+        }
+
+        @Override
+        public void setListening(boolean listening) {
+            if (listening) {
+                boolean navbarEnabled = Settings.System.getInt(resolver,
+                   Settings.Secure.NAVIGATION_BAR_ENABLED, 0) != 0;
+                String newSummary = navbarEnabled ? mContext.getString(R.string.navbar_enabled)
+                                              : mContext.getString(R.string.navbar_disabled);
+                mSummaryLoader.setSummary(this, newSummary);
+            }
+        }
+    }
+
+    public static final SummaryLoader.SummaryProviderFactory SUMMARY_PROVIDER_FACTORY
+            = new SummaryLoader.SummaryProviderFactory() {
+        @Override
+        public SummaryLoader.SummaryProvider createSummaryProvider(Activity activity,
+                                                                   SummaryLoader summaryLoader) {
+            return new SummaryProvider(activity, summaryLoader);
+        }
+    };
 }
