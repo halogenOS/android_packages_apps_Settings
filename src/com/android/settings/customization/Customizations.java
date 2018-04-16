be299044b6d5f6a9972c2b98fe19b311ca3f022f
Harsh Shandilya <harsh@prjkt.io>
2018-04-16 21:22:47 +0530

    Customizations: Add Navbar toggle [2/2]
    - Add a SwitchPreference to toggle navbar status
    - Sync navbar tuner availability with navbar
    
    Change-Id: I840512dde17ad6ba1d7861b82eebb584c87a6e5d
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- src/com/android/settings/customization/Customizations.java
+++ src/com/android/settings/customization/Customizations.java
@@ -16,25 +16,63 @@
 
 package com.android.settings.customization;
 
+import android.content.ContentResolver;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.support.v7.preference.Preference;
+import android.support.v14.preference.SwitchPreference;
+
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
-public class Customizations extends SettingsPreferenceFragment {
+public class Customizations extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
 
     private static final String TAG = Customizations.class.getSimpleName();
+    private static final String PREF_NAVBAR_TOGGLE_KEY = "navigation_bar_enabled";
+    private static final String PREF_NAVBAR_TUNER_KEY = "tuner_navbar";
+
+    private ContentResolver mResolver;
+
+    private SwitchPreference mNavbarToggle;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.customizations);
         Preference prefSet = getPreferenceScreen();
+        mResolver = getActivity().getContentResolver();
+
+        mNavbarToggle = (SwitchPreference) findPreference(PREF_NAVBAR_TOGGLE_KEY);
+        boolean mNavbarEnabled = Settings.Secure.getIntForUser(
+            mResolver, Settings.Secure.NAVIGATION_BAR_ENABLED,
+            getActivity().getResources().getBoolean(
+                com.android.internal.R.bool.config_showNavigationBar) ? 1 : 0,
+            UserHandle.USER_CURRENT) == 1;
+        mNavbarToggle.setChecked(mNavbarEnabled);
+        findPreference(PREF_NAVBAR_TUNER_KEY).setEnabled(mNavbarEnabled);
+        mNavbarToggle.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.XOS;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mNavbarToggle) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putIntForUser(mResolver,
+                Settings.Secure.NAVIGATION_BAR_ENABLED, value ? 1 : 0,
+                UserHandle.USER_CURRENT);
+            mNavbarToggle.setChecked(value);
+            findPreference(PREF_NAVBAR_TUNER_KEY).setEnabled(value);
+            return true;
+        }
+        return false;
     }
 }
