e195e8bf768c8fde0039ca3cbed6beb570db73a1
Simão Gomes Viana <devel@superboring.dev>
2024-04-21 14:02:19 +0200

    Remove more misleading error messages
    Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

--- src/com/android/settings/wifi/dpp/WifiDppQrCodeScannerFragment.java
+++ src/com/android/settings/wifi/dpp/WifiDppQrCodeScannerFragment.java
@@ -656,7 +656,6 @@ public class WifiDppQrCodeScannerFragment extends WifiDppQrCodeBaseFragment impl
             Log.e(TAG, "Invalid networkId " + newNetworkId);
             mLatestStatusCode = EasyConnectStatusCallback.EASY_CONNECT_EVENT_FAILURE_GENERIC;
             updateEnrolleeSummary();
-            showErrorMessageAndRestartCamera(R.string.wifi_dpp_check_connection_try_again);
         }
 
         @Override
@@ -772,7 +771,6 @@ public class WifiDppQrCodeScannerFragment extends WifiDppQrCodeBaseFragment impl
     @Override
     public void onFailure(int reason) {
         Log.d(TAG, "Wi-Fi connect onFailure reason - " + reason);
-        showErrorMessageAndRestartCamera(R.string.wifi_dpp_check_connection_try_again);
     }
 
     // Check is Easy Connect handshaking or not
