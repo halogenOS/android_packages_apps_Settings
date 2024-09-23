36a4b6e637f5bbe326a6026c5ec23d61631f1dac
Simão Gomes Viana <devel@superboring.dev>
2024-09-23 22:19:38 +0000

    fixup! Settings: Move adb root toggle and make it independent

--- src/com/android/settings/development/AdbRootPreferenceController.java
+++ src/com/android/settings/development/AdbRootPreferenceController.java
@@ -23,6 +23,7 @@ import android.os.UserManager;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 import androidx.preference.SwitchPreference;
+import androidx.preference.SwitchPreferenceCompat;
 
 import com.android.settings.R;
 import com.android.settings.core.PreferenceControllerMixin;
@@ -57,7 +58,7 @@ public class AdbRootPreferenceController extends DeveloperOptionsPreferenceContr
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
 
-        ((SwitchPreference) mPreference).setChecked(mADBRootService.getEnabled());
+        ((SwitchPreferenceCompat) mPreference).setChecked(mADBRootService.getEnabled());
 
         if (!isAdminUser()) {
             mPreference.setEnabled(false);
@@ -76,7 +77,7 @@ public class AdbRootPreferenceController extends DeveloperOptionsPreferenceContr
         super.onDeveloperOptionsSwitchDisabled();
 
         mADBRootService.setEnabled(false);
-        ((SwitchPreference) mPreference).setChecked(false);
+        ((SwitchPreferenceCompat) mPreference).setChecked(false);
     }
 
     @Override
