e3ac5ec7184ecd645bc35846409c602b46be23ae
Simão Gomes Viana <devel@superboring.dev>
2024-10-17 22:37:39 +0000

    Make black theme work (kinda)
    Change-Id: Ie9d52ca1084d45117a00df2505a66695773d0cd1

--- src/com/android/settings/display/BlackThemePreferenceController.java
+++ src/com/android/settings/display/BlackThemePreferenceController.java
@@ -20,8 +20,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.om.OverlayIdentifier;
 import android.content.om.OverlayInfo;
 import android.content.om.OverlayManager;
+import android.content.om.OverlayManagerTransaction;
 import android.content.res.Configuration;
 import android.os.UserHandle;
 
@@ -35,6 +37,8 @@ import com.android.settingslib.core.lifecycle.LifecycleObserver;
 import com.android.settingslib.core.lifecycle.events.OnStart;
 import com.android.settingslib.core.lifecycle.events.OnStop;
 
+import java.util.List;
+
 public class BlackThemePreferenceController extends TogglePreferenceController implements LifecycleObserver {
 
     @VisibleForTesting
@@ -42,9 +46,11 @@ public class BlackThemePreferenceController extends TogglePreferenceController i
 
     private OverlayManager mOverlayManager;
     private final String blackThemeOverlay;
+    private final Context mContext;
 
     public BlackThemePreferenceController(Context context, String key) {
         super(context, key);
+        mContext = context;
         mOverlayManager = (OverlayManager) context.getSystemService(Context.OVERLAY_SERVICE);
         blackThemeOverlay = context.getResources().getString(R.string.config_blackThemePackage);
     }
@@ -63,7 +69,24 @@ public class BlackThemePreferenceController extends TogglePreferenceController i
 
     @Override
     public boolean setChecked(boolean isChecked) {
-        mOverlayManager.setEnabled(blackThemeOverlay, isChecked, UserHandle.CURRENT);
+        // Inspired by https://github.com/DerpFest-AOSP/vendor_support/blob/3ef52d7f7c59a1885989cb86992b6b16d52c797c/src/org/derpfest/support/preferences/OverlaySwitchPreference.java#L77
+        final boolean isNight =
+            (mContext.getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK)
+                == Configuration.UI_MODE_NIGHT_YES;
+        OverlayManagerTransaction.Builder tx = new OverlayManagerTransaction.Builder();
+        tx.setEnabled(
+            mOverlayManager.getOverlayInfo(blackThemeOverlay, UserHandle.CURRENT).getOverlayIdentifier(),
+            isChecked && isNight, UserHandle.USER_CURRENT);
+
+        final List<OverlayInfo> overlays = mOverlayManager.getOverlayInfosForTarget("android", UserHandle.CURRENT);
+        for (OverlayInfo info : overlays) {
+            if ("neutral".equals(info.getOverlayName())) {
+                tx.setEnabled(info.getOverlayIdentifier(), !isChecked || !isNight, UserHandle.USER_CURRENT);
+                break;
+            }
+        }
+
+        mOverlayManager.commit(tx.build());
         return true;
     }
 
