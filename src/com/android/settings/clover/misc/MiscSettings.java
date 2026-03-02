The-Clover-Project/packages_apps_Settings

Settings: Add Google Play Integrity spoofing toggle [2/2]

src/com/android/settings/clover/misc/MiscSettings.java
@@ -1,0 +1,22 @@
+package com.android.settings.clover.misc;
+
+import android.os.Bundle;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class MiscSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.misc_settings);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CLOVER;
+    }
+}

halogenOS/android_packages_apps_Settings

Signed-off-by: Simão Gomes Viana <devel@superboring.dev>

src/com/android/settings/clover/misc/MiscSettings.java
@@ -1,0 +1,22 @@
+package com.android.settings.clover.misc;
+
+import android.os.Bundle;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class MiscSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.misc_settings);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.CUSTOM;
+    }
+}